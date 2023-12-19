2702a49ad95b1ca984d4159bb8e301952089966f
AnushArmenienne <153212278+AnushArmenienne@users.noreply.github.com>
2023-12-19 15:27:36 +0100

    initialisation du projet Planty

---
+++ app/public/wp-content/themes/blankslate-child/footer.php
@@ -0,0 +1,10 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+<div  class="copyright"><a href="http://planty.local/mentions-legales/">Mentions légales</a></div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
